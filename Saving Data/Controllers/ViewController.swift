4ee25b9c0186396e02a163b99011bee0957466c2
Christopher Smith <Chris.Software.Developer@gmail.com>
2017-11-11 14:11:58 -0800

    Added logic for saving contacts data - Refactored People to Contacts - Added fetchRequest and do try catch block - Added logic for UIAlertAction - Updated textLabel placeholer values

--- Saving Data/Controllers/ViewController.swift
+++ Saving Data/Controllers/ViewController.swift
@@ -7,32 +7,45 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
     
-    var people = [String]()
+    var contacts = [Contact]()
     
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let fetchRequest: NSFetchRequest<Contact> = Contact.fetchRequest()
+        
+        do{
+            let contacts = try PersistenceService.context.fetch(fetchRequest)
+            self.contacts = contacts
+        } catch {}
+        self.tableView.reloadData()
     }
     
     @IBAction func onPlusTapped() {
         let alert = UIAlertController(title: "Add Contact", message: nil, preferredStyle: .alert)
         alert.addTextField { (textField) in
             textField.placeholder = "Name"
-            }
+        }
         alert.addTextField { (textField) in
             textField.placeholder = "Age"
             textField.keyboardType = .numberPad
         }
         
         let action = UIAlertAction(title: "Post", style: .default) { (_) in
-            let name = alert.textFields?.first?.text
-            let age = alert.textFields?.last?.text
-            print(name)
-            print(age)
+            let name = alert.textFields!.first!.text!
+            let age = alert.textFields!.last!.text!
+            let contact = Contact(context: PersistenceService.context)
+            contact.name = name
+            contact.age = Int16(age)!
+            PersistenceService.saveContext()
+            self.contacts.append(contact)
+            self.tableView.reloadData()
         }
         
         alert.addAction(action)
@@ -46,13 +59,13 @@ extension ViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return people.count
+        return contacts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
-        cell.textLabel?.text = ""
-        cell.detailTextLabel?.text = ""
+        cell.textLabel?.text = contacts[indexPath.row].name
+        cell.detailTextLabel?.text = String(contacts[indexPath.row].age)
         return cell
     }
 }
